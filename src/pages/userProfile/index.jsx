6691ef0f21f03aacca4e61fe9232baddfed3462d
Rishab <rishabkanaujia2002@gmail.com>
2024-03-03 23:16:51 +0530

    sixth commit

--- src/pages/userProfile/index.jsx
+++ src/pages/userProfile/index.jsx
@@ -9,10 +9,12 @@ import { doSignOut } from "../../firebase/auth";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/authContext";
 
+
 const UserProfile = () => {
-  const {userLoggedIn} = useAuth();
+  const {userLoggedIn,currentUser} = useAuth();
   const navigate = useNavigate();
   const { id } = useParams();
+  const [isUser, setIsUser] = useState(true)
   const [user, setUser] = useState({
     name: "",
     phone: "",
@@ -22,12 +24,21 @@ const UserProfile = () => {
     dob: "",
   });
 
+  const checkUserProfile = async ()=>{
+    const response = await getUserfromId(currentUser.uid);
+      
+    if (response && response.data==="") {
+     
+        navigate("/createProfile")
+      
+    }
+  }
   const getuser = async () => {
     const response = await getUserfromId(id);
     
     if (response) {
       if(response.data===""){
-        navigate("/createProfile")
+       setIsUser(false)
       }
       setUser(response.data);
     }
@@ -43,6 +54,7 @@ const UserProfile = () => {
   };
 
   useEffect(() => {
+    checkUserProfile();
     setQrcode();
     getuser();
   }, []);
@@ -59,38 +71,40 @@ const UserProfile = () => {
 
 
   return (
-    <div className={styles.userProfile}>
-      <div className={styles.navbar}>
-        {userLoggedIn?<button onClick={logout}>Logout</button>:<button onClick={login}>Login</button>}
-        
-      </div>
-      <img className={styles.coverImg} src={user.coverPhoto} alt="" />
-      <img className={styles.profileImg} src={user.profilePhoto} alt="" />
-      <h2>{user.name}</h2>
+    <div>
+    {!isUser ? <h1>User Not Found</h1> : <div className={styles.userProfile}>
+    <div className={styles.navbar}>
+      {userLoggedIn?<button onClick={logout}>Logout</button>:<button onClick={login}>Login</button>}
+      
+    </div>
+    <img className={styles.coverImg} src={user.coverPhoto} alt="" />
+    <img className={styles.profileImg} src={user.profilePhoto} alt="" />
+    <h2>{user.name}</h2>
 
-      <div className={styles.main}>
-        <div className={styles.userInfo}>
-          <FlipBox
-            data={{ label: "Gender", value: user.gender }}
-            color1={"#0081D0"}
-            color2={"#008F79"}
-          />
-          <FlipBox
-            data={{ label: "Phone", value: user.phone }}
-            color1={"#4B4453"}
-            color2={"#D03C2A"}
-          />
-          <FlipBox
-            data={{ label: "D.O.B", value: user.dob }}
-            color1={"#005245"}
-            color2={"#845EC3"}
-          />
-        </div>
-        <div className={styles.qrHolder}>
-          <InteractiveImage src={qrSrc} />
-        </div>
+    <div className={styles.main}>
+      <div className={styles.userInfo}>
+        <FlipBox
+          data={{ label: "Gender", value: user.gender }}
+          color1={"#0081D0"}
+          color2={"#008F79"}
+        />
+        <FlipBox
+          data={{ label: "Phone", value: user.phone }}
+          color1={"#4B4453"}
+          color2={"#D03C2A"}
+        />
+        <FlipBox
+          data={{ label: "D.O.B", value: user.dob }}
+          color1={"#005245"}
+          color2={"#845EC3"}
+        />
+      </div>
+      <div className={styles.qrHolder}>
+        <InteractiveImage src={qrSrc} />
       </div>
     </div>
+  </div>}
+  </div>
   );
 };
 
